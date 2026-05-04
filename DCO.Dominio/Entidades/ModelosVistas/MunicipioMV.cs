9031c4060ec7d205920c3a3a816e3aa412c987cf
ZEUS\ediso <edisongim@hotmail.com>
2026-05-03 21:37:37 -0500

    PBIXXXX _ Creación de modelo a interface para obtener municipios, listas de municipios.

---
+++ DCO.Dominio/Entidades/ModelosVistas/MunicipioMV.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DCO.Dominio.Entidades.ModelosVistas
+{
+    public class MunicipioMV
+    {
+        public int PaisId { get; set; }
+        public string CodigoPais { get; set; } = null!;
+        public string NombrePais { get; set; } = null!;
+        public string IndicativoPais { get; set; } = null!;
+        public bool EstadoPais { get; set; }
+
+        public int DepartamentoId { get; set; }
+        public string CodigoDepartamento { get; set; } = null!;
+        public string NombreDepartamento { get; set; } = null!;
+        public string IndicativoDepartamento { get; set; } = null!;
+        public bool EstadoDepartamento { get; set; }
+
+        public int MunicipioId { get; set; }
+        public string CodigoMunicipio { get; set; } = null!;
+        public string NombreMunicipio { get; set; } = null!;
+        public bool EstadoMunicipio { get; set; }
+
+
+    }
+}
---
+++ DCO.Dominio/Repositorio/IMunicipioRepositorio.cs
@@ -0,0 +1,11 @@
+﻿using DCO.Dominio.Entidades.ModelosVistas;
+
+namespace DCO.Dominio.Repositorio
+{
+    public interface IMunicipioRepositorio
+    {
+        Task<MunicipioMV> ObtenerUbicacionPorMunicipioId(int municipioId);
+        Task<MunicipioMV> ObtenerUbicacionPorCodigoDepartamentoMunicipio(string codigoDepartamento, string codigoMunicipio);
+        IQueryable<MunicipioMV> ListarUbicacionesPorDepartamentoId(int departamentoId);
+    }
+}

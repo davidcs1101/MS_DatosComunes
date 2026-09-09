a056fb04590997e19e3402da0add79cc05e7b717
ZEUS\ediso <edisongim@hotmail.com>
2026-09-09 01:06:27 -0500

    no message

---
+++ DCO.Api.DatosComunes/Controllers/RecepcionEventoController.cs
@@ -0,0 +1,29 @@
+﻿using DCO.Aplicacion.CasosUso.Interfaces;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Utilidades.Dtos;
+using Utilidades.Seguridad;
+
+namespace ApiDCO.Controllers
+{
+    [ApiController]
+    [Route("api/recepcionEventos")]
+    [Authorize(Policy = Politicas.GRUPOSFUNCIONESSISTEMA)]
+    public class RecepcionEventoController : Controller
+    {
+        private readonly IColaSolicitudServicio _colaSolicitudServicio;
+        public RecepcionEventoController(IColaSolicitudServicio colaSolicitudServicio)
+        {
+            _colaSolicitudServicio = colaSolicitudServicio; 
+        }
+
+        [HttpPost("recibirEvento")]
+        public async Task<ActionResult<ApiResponseDto<int>>> RecibirEvento(ColaSolicitudCreacionRequest colaSolicitudCreacionRequest)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest(ModelState);
+
+            return await _colaSolicitudServicio.CrearAsync(colaSolicitudCreacionRequest);
+        }
+    }
+}
--- DCO.Aplicacion/CasosUso/Interfaces/IColaSolicitudServicio.cs
+++ DCO.Aplicacion/CasosUso/Interfaces/IColaSolicitudServicio.cs
@@ -1,8 +1,14 @@
-﻿namespace DCO.Aplicacion.CasosUso.Interfaces
+﻿using DCO.Dominio.Entidades;
+using Utilidades.Dtos;
+
+namespace DCO.Aplicacion.CasosUso.Interfaces
 {
     public interface IColaSolicitudServicio
     {
         Task ProcesarColaSolicitudesAsync();
         Task ProcesarPorColaSolicitudIdAsync(int id, bool validarEstadoPendiente = false);
+        Task<ApiResponseDto<int>> CrearAsync(ColaSolicitudCreacionRequest colaSolicitudCreacionRequest);
+        Task<DCO_ColaSolicitud> AgregarColaSolicitud(string tipo, object payload, string urlDestino = "");
+        Task<List<DCO_ColaSolicitud>> AgregarColasSolicitudes(string tipo, object payload, List<string?>? urlsDestino = null);
     }
 }
